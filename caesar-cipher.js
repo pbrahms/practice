9f8dfd8f7f79cfa13c3a80f22bbfd5680634aa0d
Filip Abramovic <filip.v.abramovic@gmail.com>
2020-11-27 23:09:30 +0100

    update

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -7,7 +7,7 @@ let isLowerCase = function(char) {
     }
 }
 
-let caesar = function(word, key) {
+let caesarEncrypt = function(word, key) {
     let origAr = word.split('');
     let crypAr = [];
     key = key % 25;
@@ -34,5 +34,36 @@ let caesar = function(word, key) {
     return crypSt;
 }
 
-let x = caesar('XZYabcDE', 1);
+let caesarDecrypt = function(word, key) {
+    let origAr = word.split('');
+    let crypAr = [];
+    key = key % 25;
+    key = key * -1;
+
+    for(let i=0; i<word.length; i++) {
+        let n = origAr[i].toUpperCase().charCodeAt(0) + key;
+        if(n > 90) {
+            let p = n - 90;
+            crypAr.push(String.fromCharCode(64 + p));
+        }
+        else if (n < 65) {
+            let p = 65 - n;
+            crypAr.push(String.fromCharCode(91 - p));
+        }
+        else {
+            crypAr.push(String.fromCharCode(n));
+        }
+    }
+    for(let i=0; i<word.length; i++){
+        if(isLowerCase(origAr[i])) {
+            crypAr[i] = crypAr[i].toLowerCase();
+        }
+    }
+    let crypSt = crypAr.join('');
+    return crypSt;
+}
+
+let x = caesarEncrypt('Filip', 1);
+let y = caesarDecrypt('Gjmjq', 1);
 console.log(x);
+console.log(y);
