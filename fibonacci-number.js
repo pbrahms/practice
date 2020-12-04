dae8f143f236befe6a70ee7133356ed9909f1b8c
Filip Abramovic <filip.v.abramovic@gmail.com>
2020-12-04 16:47:49 +0100

    recursive-function
    fibonacci - added recursive function solution

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -1,3 +1,5 @@
+//solution 1
+/*
 let fibonacci = function (index) {
     let num1 = 0,
         num2 = 1,
@@ -15,6 +17,28 @@ let fibonacci = function (index) {
             num2 = num3;
         }
         return num3;
+    }
+}
+console.log(fibonacci(8));
+*/
+
+//solution 2 - recursive function
+
+let fibonacci = function (index) {
+    let num0 = 0,
+        num1 = 1,
+        num2;
+    if (index === 0) {
+        return num0;
+    }
+    else if (index === 1) {
+        return num1;
+    }
+    else {
+        for (let i = 2; i <= index; i++) {
+            num2 = fibonacci(index - 1) + fibonacci(index - 2);
+        }
+        return num2;
     }
 }
 console.log(fibonacci(8));
